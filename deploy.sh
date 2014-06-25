9100e7ebac368cf6324bb0974d651a110c1da92f
Stuart Watt <stuart@morungos.com>
2014-06-25 10:33:45 -0400

    Modified deployment examples to use the provisioning variable

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-#ansible-playbook -v -i .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory --private-key=~/.vagrant.d/insecure_private_key -u vagrant global.yml
+#ansible-playbook -v -i .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory --private-key=~/.vagrant.d/insecure_private_key -u vagrant global.yml --extra-vars "provisioning_platform=virtualbox"
 #ansible-playbook -v -i .vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory --private-key=~/.ssh/stuart-oicr-1.pem -u ubuntu global.yml --extra-vars "provisioning_platform=aws"
